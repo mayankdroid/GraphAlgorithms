2c40c87193d0b3911f9f1041403c80e1fa935090
Gleb Efimov <efimov.gleb@gmail.com>
2013-06-25 00:39:43 +0300

    [J] Hamilton path. WA-2

--- [J]HamiltonPath/src/HamiltonPath.java
+++ [J]HamiltonPath/src/HamiltonPath.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
@@ -6,19 +7,6 @@ public class HamiltonPath implements Runnable {
     StringTokenizer st;
     BufferedReader in;
     PrintWriter out;
-
-    final int MAX_VERTICES = 10000;
-    final int MAX_EDGES = 100000;
-
-    final int[][] adjacency = new int[4][MAX_VERTICES + MAX_EDGES + 1];
-    final int[] colorVertex = new int[MAX_VERTICES + 1];
-    final int[] longestPath = new int[MAX_VERTICES + 1];
-
-    static int vertices, edges, neededLength = 0;
-
-    int[] startVertices = new int[MAX_VERTICES + 1];
-    int[] path = new int[MAX_VERTICES + 1];
-
 
     public static void main(String[] args) {
         new Thread(new HamiltonPath()).start();
@@ -57,17 +45,72 @@ public class HamiltonPath implements Runnable {
         return Double.parseDouble(nextToken());
     }
 
+    final private int MAX_VERTICES = 2368;
+    final private int MAX_EDGES = MAX_VERTICES * (MAX_VERTICES - 1);
+
+    private int vertices, edges;
+    private boolean pathFinded, flagNext;
+    final private int[][] adjacency = new int[3][MAX_VERTICES + 2 * MAX_EDGES + 1];
+    final private int[] path = new int[MAX_VERTICES];
+    final private boolean[] color = new boolean[MAX_VERTICES + 1];
+    final private boolean[] color1 = new boolean[MAX_VERTICES + 1];
+
     void solve() throws NumberFormatException, IOException {
         vertices = nextInt();
         edges = nextInt();
         readAdjacencyList();
-
         for (int i = 1; i <= vertices; i++) {
-            dfs(i);
+            dfs(i, 1);
+            Arrays.fill(color, false);
+            flagNext = false;
+            if (pathFinded) break;
+        }
+        for (int i = 1; i <= vertices; i++) {
+            out.print(path[i] + " ");
         }
     }
 
-    private void dfs(int vertex) {
+    private void dfs(int vertex, int depth) {
+        color[vertex] = true;
+        path[depth] = vertex;
+        if (depth == vertices) {
+            pathFinded = true;
+            return;
+        }
+        int achievableTop = F(vertex);
+        Arrays.fill(color1, false);
+        if (depth + achievableTop < vertices - 1) {
+            flagNext = true;
+            color[vertex] = false;
+            return;
+        }
+        int index = vertex;
+        while (adjacency[1][index] != 0) {
+            index = adjacency[1][index];
+            if (color[adjacency[0][index]]) {
+                continue;
+            }
+            dfs(adjacency[0][index], depth + 1);
+            if (pathFinded || flagNext) {
+                color[vertex] = false;
+                return;
+            }
+        }
+        color[vertex] = false;
+    }
+
+    private int F(int vertex) {
+        color1[vertex] = true;
+        int result = 0;
+        int index = vertex;
+        while (adjacency[1][index] != 0) {
+            index = adjacency[1][index];
+            if (!color[adjacency[0][index]] && !color1[adjacency[0][index]]) {
+                result += F(adjacency[0][index]);
+                result++;
+            }
+        }
+        return result;
     }
 
     void readAdjacencyList() throws NumberFormatException, IOException {
@@ -77,13 +120,16 @@ public class HamiltonPath implements Runnable {
             adjacency[1][i] = 0;
             adjacency[2][i] = i;
         }
-        for (int i = vertices + 1; i <= vertices + edges; i++) {
+        for (int i = vertices + 1; i <= vertices + edges * 2; i += 2) {
             from = nextInt();
             to = nextInt();
             adjacency[0][i] = to;
             adjacency[1][adjacency[2][from]] = i;
             adjacency[2][from] = i;
-            adjacency[3][i]++;
+
+            adjacency[0][i + 1] = from;
+            adjacency[1][adjacency[2][to]] = i + 1;
+            adjacency[2][to] = i + 1;
         }
     }
 }
